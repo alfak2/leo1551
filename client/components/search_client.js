ddf0f65f1b86a2aeb91d707535e0e100619cb388
leoriver <barcopetin@gmail.com>
2016-06-28 21:16:02 +0200

    Revert "leo1551"
    This reverts commit 7adaa5f83bffa792fad45561632993bbe106733d.

--- client/components/search_client.js
+++
@@ -1,7 +0,0 @@
-    var options = {
-        keepHistory: 1000 * 60 * 5,
-        localSearch: true
-    };
-    var fields = ['title'];
-
-    Search = new SearchSource('listings', fields, options);
--- client/test_data/insert_images.js
+++
@@ -1,5 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(function(){
-  Meteor.call('seedImages'); //Insert test data images. Please remove on production.
-})
